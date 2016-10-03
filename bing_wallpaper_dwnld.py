a869bf2a1fcce06fa57e6cc5ee1e64011ba2a39e
Akshay Karangale <akshaykarangale@gmail.com>
2016-10-03 13:26:39 +0530

    Update bing_wallpaper_dwnld.py

--- bing_wallpaper_dwnld.py
+++ bing_wallpaper_dwnld.py
@@ -13,38 +13,40 @@ import time
 # In[40]:
 code = 400
 print("Downloading wallpaper....")
+# waiting for my laptop to get connected to internet when my PC starts
 while code != 200:
 	time.sleep(15)
-	fn = datetime.date.isoformat(datetime.datetime.now()).replace("-","")
+	fn = datetime.date.isoformat(datetime.datetime.now()).replace("-","") # building filename
 	try:
-		response = requests.get("http://bingwallpaper.com/"+fn+".html")
+		response = requests.get("http://bingwallpaper.com/"+fn+".html") # getting html page from url
 # In[3]:
 
-		soup = bs4.BeautifulSoup(response.text, 'lxml')
+		soup = bs4.BeautifulSoup(response.text, 'lxml') # parsing the html
 
 
 # In[8]:
 
-		a = soup.select('.imgContainer > img')
+		a = soup.select('.imgContainer > img') # getting the link of today's wallpaper
 
 
 # In[18]:
 
-		url = "http://"+a[0]['src'][2:]
+		url = "http://"+a[0]['src'][2:] # building the url for downloading wallpaper
 
 
 # In[37]:
 # In[38]:
 
-		urllib.request.urlretrieve(url,filename="/home/akshay/bing_wallpaper/"+fn+".jpg")
+		urllib.request.urlretrieve(url,filename="/home/akshay/bing_wallpaper/"+fn+".jpg") # downloading the wallpaper
 		print("Downloading complete !")
 
 # In[39]:
-
+		# setting the wallpaper
 		os.system("gsettings set org.gnome.desktop.background picture-uri file:///home/akshay/bing_wallpaper/"+fn+".jpg")
 		print("Wallpaper set")
 		code = 200
 	except requests.exceptions.ConnectionError:
+		# catching the exception when internet is not connected
 		print("Internet not connected")
 # In[ ]:
 
